edeaef7271d55701e76c5eda335a13a58eb78b60
Philipp Seelig <philipp@daxbau.net>
2023-09-04 10:14:05 +0200

    use bash instead of sh   bsky script uses bash so it is available and bash has more features/failsaves then sh fail the script on command returning error code use variables to make code more human readable surround debug output of the TWEET variable with ' so that leading, trailing white spaces are visible

--- mythologybot.sh
+++ mythologybot.sh
@@ -1,7 +1,19 @@
-#!/usr/bin/env sh
-. ~/.cronrc;
-export TWEET=$(awk NR==$((${RANDOM} % `wc -l < motifs.txt` + 1)) motifs.txt | sed -r 's/(^\S+? )|(: |;|,|\?|!)| ([\(\"].{3,}[\)\"]) |\b(--|int?o?|about|while|he|she|they|s?o? ?that|as|who|by|when|for|witho?u?t?|at|on|and|because|but|to b?e?|from|under|has|where|how many|how long|of|or|whiche?v?e?r?)\b/\1\2\n\3\4/g');
-echo -en Should tweet\n $TWEET;
+#!/usr/bin/env bash
+
+# fail the execution of the script on command returning error
+set -e
+
+. ~/.cronrc
+
+file_length=$(wc -l < motifs.txt)
+# get random line number
+random_line_number=$((${RANDOM} % ${file_length} + 1))
+
+export TWEET=$(awk NR==$random_line_number motifs.txt | sed -r 's/(^\S+? )|(: |;|,|\?|!)| ([\(\"].{3,}[\)\"]) |\b(--|int?o?|about|while|he|she|they|s?o? ?that|as|who|by|when|for|witho?u?t?|at|on|and|because|but|to b?e?|from|under|has|where|how many|how long|of|or|whiche?v?e?r?)\b/\1\2\n\3\4/g')
+
+# debug output
+echo -e "Should tweet\n\t'$TWEET'"
+
 twurl set default MythologyBot
 twurl '/2/tweets' --data '{"text": "'"${TWEET//$'\n'/\\n}"'"}' --header 'Content-Type: application/json' --consumer-key ${MYTHBOT_CONSUMERKEY} --consumer-secret ${MYTHBOT_CONSUMERSECRET} --access-token ${MYTHBOT_ACCESSTOKEN} --token-secret ${MYTHBOT_TOKENSECRET}
 curl -H "Authorization: Bearer $MYTHOLOGYBOT_MASTODON" -d "status=$TWEET" https://botsin.space/api/v1/statuses
