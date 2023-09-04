827292e4b74415bc02d8d1dc084f70c43aed22ba
Philipp Seelig <philipp@daxbau.net>
2023-09-04 11:27:13 +0200

    use the same improvements as in mythologybot.sh

--- mythologybutt.sh
+++ mythologybutt.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
 . ~/.cronrc;
-TWEET=$(awk NR==$((${RANDOM} % `wc -l < new_motifs.txt` + 1)) new_motifs.txt | sed -r 's/(^\S+? )|(: |;|,|\?|!)| ([\(\"].{3,}[\)\"]) |\b(--|int?o?|about|while|he|she|they|s?o? ?that|as|who|by|when|for|witho?u?t?|at|on|and|because|but|to b?e?|from|under|has|where|of|or|whiche?v?e?r?)\b/\1\2\n\3\4/g');
-echo Should tweet\n $TWEET;
+
+set -e
+
+file_length=$(wc -l < motifs.txt)
+# get random line number
+random_line_number=$((${RANDOM} % ${file_length} + 1))
+
+TWEET=$(awk NR==$random_line_number new_motifs.txt | sed -r 's/(^\S+? )|(: |;|,|\?|!)| ([\(\"].{3,}[\)\"]) |\b(--|int?o?|about|while|he|she|they|s?o? ?that|as|who|by|when|for|witho?u?t?|at|on|and|because|but|to b?e?|from|under|has|where|of|or|whiche?v?e?r?)\b/\1\2\n\3\4/g');
+
+# debug output
+echo -e "Should tweet\n\t'$TWEET'"
+
 twurl set default MythologyButt
 twurl tweet -d "status=$TWEET" /1.1/statuses/update.json;
 curl -H "Authorization: Bearer $MYTHOLOGYBUTT_MASTODON" -d "status=$TWEET" https://botsin.space/api/v1/statuses
